375b1c5e746ab38da039ca40e9bdd44bf2a0051b
koudai0024 <iwai.0731v@gmail.com>
2021-03-11 20:40:37 +0900

    :lipstick: Add Typography Component

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { CommonContainer } from "src/components/Sheard/CommonContainer";
+import { MainHeading } from "src/components/Sheard/Typography";
+
+const Home = () => {
+  return (
+    <CommonContainer>
+      <MainHeading variant="h1">トレンド</MainHeading>
+      <div></div>
+    </CommonContainer>
+  );
+};
+
+export default Home;
