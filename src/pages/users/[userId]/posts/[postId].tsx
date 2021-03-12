a32e72451f6e6c656878bc70441f3665e747a0fe
koudai0024 <iwai.0731v@gmail.com>
2021-03-12 21:16:35 +0900

    :lipstick: Add Dashboard Page

---
+++ src/pages/users/[userId]/posts/[postId].tsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import { format } from "date-fns";
+import hljs from "highlight.js";
+import marked from "marked";
+import type { GetStaticPaths, GetStaticProps } from "next";
+import type { VFC } from "react";
+
+type Props = {
+  post: PostType;
+};
+
+const PostPage: VFC<Props> = ({ post }) => {
+  marked.setOptions({
+    headerIds: false,
+  });
+  const $ = cheerio.load(marked(post.body));
+  $("a").each((_, elm) => {
+    $(elm).attr("rel", "noopener noreferrer");
+    $(elm).attr("target", "_blank");
+  });
+  $("img").each((_, elm) => {
+    $(elm).attr("loading", "lazy");
+  });
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+  return (
+    <div>
+      <div className="w-full xl:w-11/12 max-w-screen-xl text-center mx-auto mt-4 md:mt-8 mb-3 md:mb-6 px-2 xl:px-0">
+        <h1 className="inline-block text-xl md:text-3xl font-bold text-left mb-2 md:mb-4">
+          {post.title}
+        </h1>
+        <p className="text-xs md:text-base font-bold mb-1 md:mb-2">
+          {post.user.name}
+        </p>
+        <p className="text-xs md:text-base text-gray-800">
+          {format(new Date(post.createdAt), "yyyy年M月d日")}
+        </p>
+      </div>
+      <div
+        className="markdown-body bg-white w-full max-w-screen-xl mx-auto shadow px-1 md:px-2 py-2 md:py-4"
+        dangerouslySetInnerHTML={{ __html: $.html() }}
+      ></div>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await axios.get("/api/v1/posts?limit=-1");
+  const { posts } = await res.data;
+  const paths: Array<string> = posts.map((repo: UserType) => {
+    return `/users/[user_id]/posts/${repo.id}`;
+  });
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const res = await axios.get(`/api/v1/posts/${ctx.params?.postId}`);
+  const post = await res.data;
+  return {
+    props: { post },
+    revalidate: 60,
+  };
+};
+
+export default PostPage;
