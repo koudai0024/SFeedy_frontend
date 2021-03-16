fbf0947b1bb7ef93bcbfd11bfb54ac54ae0cd941
koudai0024 <iwai.0731v@gmail.com>
2021-03-16 17:01:46 +0900

    :lipstick: Editor

--- src/components/Sheard/Editor.tsx
+++ src/components/Sheard/Editor.tsx
@@ -1,5 +1,3 @@
-import { faImage } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import cheerio from "cheerio";
 import cc from "classcat";
@@ -30,6 +28,12 @@ export const Editor: VFC<Props> = ({
   const [isLoad, setIsLoad] = useState(false); //Todo Loading Icon 追加
   const user = useRecoilValue(userState);
 
+  const [isMode, setIsMode] = useState<"edit" | "preview">("edit");
+
+  const handleMode = () => {
+    isMode === "edit" ? setIsMode("preview") : setIsMode("edit");
+  };
+
   const handleImage = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files?.[0]) {
       setIsLoad(false);
@@ -53,7 +57,6 @@ export const Editor: VFC<Props> = ({
       });
     return;
   };
-
   const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
@@ -96,9 +99,9 @@ export const Editor: VFC<Props> = ({
         onChange={handleTags}
         className="bg-white text-sm md:text-base w-full h-10 rounded p-2 mb-2"
       />
-      <div className="bg-white flex">
-        <div className="box-border overflow-hidden w-2/4 border-r border-black">
-          <div className="block w-full border-b border-black px-2 py-1">
+      <div className="bg-white flex border-t border-b border-black ">
+        <div className="box-border order-2 md:order-1 overflow-hidden w-2/4 md:border-r border-black">
+          <div className="flex justify-end md:justify-start w-full px-2 py-1">
             <label
               htmlFor="photoBtn"
               className={cc([
@@ -107,7 +110,18 @@ export const Editor: VFC<Props> = ({
                 },
               ])}
             >
-              <FontAwesomeIcon icon={faImage} />
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                  clipRule="evenodd"
+                />
+              </svg>
               <input
                 className="hidden"
                 type="file"
@@ -118,14 +132,47 @@ export const Editor: VFC<Props> = ({
             </label>
           </div>
         </div>
-        <div className="overflow-hidden w-2/4">
-          <div className="w-full border-b border-black px-2 py-1">
+        <div className="overflow-hidden w-2/4 order-1">
+          <div className="hidden md:block w-full px-2 py-1">
             <p>Preview</p>
+          </div>
+          <div className="md:hidden w-full px-2 py-1">
+            <button
+              className={cc([
+                "text-sm rounded px-2 py-0.5 mr-1",
+                {
+                  "bg-pink-500 text-white": isMode !== "preview",
+                  "bg-gray-200 pointer-events-none": isMode === "preview",
+                },
+              ])}
+              onClick={handleMode}
+            >
+              Preview
+            </button>
+            <button
+              className={cc([
+                "text-sm rounded px-2 py-0.5 ",
+                {
+                  "bg-pink-500 text-white": isMode !== "edit",
+                  "bg-gray-200 pointer-events-none": isMode === "edit",
+                },
+              ])}
+              onClick={handleMode}
+            >
+              Edit
+            </button>
           </div>
         </div>
       </div>
       <div className="bg-white flex h-content mb-4">
-        <div className="box-border overflow-hidden w-2/4 h-full border-r border-black">
+        <div
+          className={cc([
+            "box-border overflow-hidden w-full md:w-2/4 h-full md:border-r border-black",
+            {
+              "hidden md:block": isMode !== "edit",
+            },
+          ])}
+        >
           <textarea
             placeholder="本文を入力してください"
             value={body}
@@ -138,7 +185,14 @@ export const Editor: VFC<Props> = ({
             ])}
           ></textarea>
         </div>
-        <div className="overflow-hidden w-2/4 h-full">
+        <div
+          className={cc([
+            "overflow-hidden w-full md:w-2/4 h-full",
+            {
+              "hidden md:block": isMode !== "preview",
+            },
+          ])}
+        >
           <div
             className="markdown-body overflow-scroll w-full h-full p-4 "
             dangerouslySetInnerHTML={{ __html: $.html() }}
