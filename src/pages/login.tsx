b82a084982b0493badd4b4f28796b3b6d14ad9c9
koudai0024 <iwai.0731v@gmail.com>
2021-03-27 14:32:53 +0900

    :fire:

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,47 +12,6 @@ import { userState } from "src/lib/atom";
 const Login = () => {
   const setUser = useSetRecoilState(userState);
   const router = useRouter();
-  // const handleLogin = () => {
-  //   let accessToken: string;
-  //   const childwin = window.open(
-  //     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/google`
-  //   );
-  //   window.addEventListener("message", (e) => {
-  //     if (e.data.message) {
-  //       accessToken = e.data.message;
-  //     }
-  //   });
-  //   const timeId = setInterval(() => {
-  //     if (childwin?.closed && accessToken !== "") {
-  //       clearInterval(timeId);
-  //       setCookie(null, "auth", accessToken);
-  //       axios
-  //         .get("/api/v1/users/info", {
-  //           headers: {
-  //             Authorization: `Bearer ${accessToken}`,
-  //           },
-  //         })
-  //         .then((res) => {
-  //           setUser({
-  //             id: res.data.id,
-  //             name: res.data.name,
-  //             email: res.data.email,
-  //             image: res.data.image,
-  //             accessToken: accessToken,
-  //           });
-  //           setCookie(
-  //             null,
-  //             "user",
-  //             `${JSON.stringify({ ...res.data, accessToken })}`
-  //           );
-  //           router.push("/");
-  //         })
-  //         .catch(() => {
-  //           alert("login error");
-  //         });
-  //     }
-  //   }, 1000);
-  // };
 
   const handleGoogle = (
     res: GoogleLoginResponse | GoogleLoginResponseOffline
